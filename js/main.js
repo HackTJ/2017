40224b8f7fe59e9a76ef385f247051cf19abfb00
Peter Andringa <peter@andrin.ga>
2015-11-02 14:57:46 -0500

    connect to API

--- js/main.js
+++ js/main.js
@@ -25,6 +25,10 @@ var button = document.querySelector('button.submit');
 var icon = document.querySelector('button.submit .icon');
 var confirmation = document.querySelector('.confirmation-text');
 
+// Regular Expressions
+var phoneNumber = /^\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
+var emailAddress = /^[^@]*@[a-z\-._]{1,}\.[a-z]{2,}$/i;
+
 // Event Listeners
 var validate = function(e){
 	if ( hasClass(button, 'is-disabled') && input.value ) {
@@ -32,8 +36,18 @@ var validate = function(e){
 	} else if( !hasClass(button, 'is-disabled') && !input.value && !hasClass(button, 'is-done') ) {
 		addClass(button, 'is-disabled');
 	}
+
 	if ( hasClass(button, 'is-done') ) {
 		removeClass(button, 'is-done');
+		removeClass(icon, 'icon-check');
+	}
+	if ( hasClass(confirmation, 'is-shown') ) {
+		removeClass(confirmation, 'is-shown');
+	}
+
+	// Do nothing if the user presses 
+	if(event.keyCode === 13){
+		submit({});
 	}
 }
 
@@ -42,9 +56,19 @@ input.addEventListener('focus', validate);
 input.addEventListener('blur', validate);
 
 var submit = function(e){
+	console.log('submit');
 	addClass(button, 'is-done');
 
 	var type = "invalid";
+
+	var value = input.value.trim();
+
+
+	if( value.match(phoneNumber) ) {
+		type = "phone"
+	}else if( value.match(emailAddress) ) {
+		type = "email";
+	}
 
 	if(type !== "email" && type !== "phone"){
 		confirmation.innerHTML = "Uh oh - that doesn't look like a phone number or email address.";
@@ -52,18 +76,40 @@ var submit = function(e){
 
 		addClass(icon, 'icon-close-circle');
 	} else {
-		addClass(icon, 'icon-spin');
 		addClass(icon, 'icon-config');
-		setTimeout(function(){
+		addClass(icon, 'icon-spin');
+		
+		// Universal Callback
+		var cb = function(err, message) {
 			removeClass(icon, 'icon-spin');
 			removeClass(icon, 'icon-config');
 			
-			addClass(icon, 'icon-check');
-			addClass(input, 'is-done');
+			if(err) {
+				addClass(icon, 'icon-close-circle');
+			} else {
+				addClass(icon, 'icon-check');
+			}
 
-			confirmation.innerHTML = "We'll send you " + (type == "phone" ? "a text message" : "an email") + ".";
+			confirmation.innerHTML = message;
 			addClass(confirmation, 'is-shown');
-		}, 1000);
+		}
+		apiRequest(value, type, cb);
 	}
+	return false;
 }
+
+function apiRequest(value, type, callback){
+	var request = new XMLHttpRequest();
+	request.open("POST", "https://api.hacktj.org/interest/"+type);
+	request.onreadystatechange = function() {
+    if (this.readyState == 4) {
+    	return callback(this.status!==200, this.responseText);
+    }
+	}
+	var data = {};
+	data[type] = value;
+	request.setRequestHeader("Content-Type", "application/json");
+	request.send(JSON.stringify(data));
+}
+
 button.addEventListener('click', submit);
