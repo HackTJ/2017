5783ed7c579a9724b6581a74c7055a8daddb7655
Peter Andringa <peter@andrin.ga>
2015-11-14 15:42:28 -0500

    hero, intro, and FAQ

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 // Helpers
 function hasClass(el, className) {
+  if (!el) return false;
   if (el.classList)
     return el.classList.contains(className)
   else
     return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
 }
 function addClass(el, className) {
+  if (!el) return false;
   if (el.classList)
     el.classList.add(className)
   else if (!hasClass(el, className)) el.className += " " + className
 }
 function removeClass(el, className) {
+  if (!el) return false;
   if (el.classList)
     el.classList.remove(className)
   else if (hasClass(el, className)) {
@@ -19,97 +22,32 @@ function removeClass(el, className) {
   }
 }
 
-// Elements
-var input = document.querySelector('input.contact');
-var button = document.querySelector('button.submit');
-var icon = document.querySelector('button.submit .icon');
-var confirmation = document.querySelector('.confirmation-text');
+// FAQ Section
+var faqContainer = document.querySelector('.faq-container');
+var pageGroup = document.querySelector('ul.page-group');
+var faqNav = document.querySelector('.faq-nav')
+var pages = Array.prototype.slice.call(document.querySelectorAll('.faq-container .page'));
+var tabs = document.querySelectorAll('.faq-nav .nav-item');
+var underline = document.querySelector('.underline');
+var tabWidth = tabs.item(0).clientWidth
+underline.style.width = tabWidth+"px"
 
-// Regular Expressions
-var phoneNumber = /^\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
-var emailAddress = /^[^@]*@[a-z\-._]{1,}\.[a-z]{2,}$/i;
-
-// Event Listeners
-var validate = function(e){
-	if ( hasClass(button, 'is-disabled') && input.value ) {
-		removeClass(button, 'is-disabled');
-	} else if( !hasClass(button, 'is-disabled') && !input.value && !hasClass(button, 'is-done') ) {
-		addClass(button, 'is-disabled');
-	}
-
-	if ( hasClass(button, 'is-done') ) {
-		removeClass(button, 'is-done');
-		removeClass(icon, 'icon-check');
-	}
-	if ( hasClass(confirmation, 'is-shown') ) {
-		removeClass(confirmation, 'is-shown');
-	}
-
-	// Do nothing if the user presses 
-	if(event.keyCode === 13){
-		submit({});
-	}
-}
-
-input.addEventListener('keyup', validate);
-input.addEventListener('focus', validate);
-input.addEventListener('blur', validate);
-
-var submit = function(e){
-	console.log('submit');
-	addClass(button, 'is-done');
-
-	var type = "invalid";
-
-	var value = input.value.trim();
-
-
-	if( value.match(phoneNumber) ) {
-		type = "phone"
-	}else if( value.match(emailAddress) ) {
-		type = "email";
-	}
-
-	if(type !== "email" && type !== "phone"){
-		confirmation.innerHTML = "Uh oh - that doesn't look like a phone number or email address.";
-		addClass(confirmation, 'is-shown');
-
-		addClass(icon, 'icon-close-circle');
-	} else {
-		addClass(icon, 'icon-config');
-		addClass(icon, 'icon-spin');
-		
-		// Universal Callback
-		var cb = function(err, message) {
-			removeClass(icon, 'icon-spin');
-			removeClass(icon, 'icon-config');
-			
-			if(err) {
-				addClass(icon, 'icon-close-circle');
-			} else {
-				addClass(icon, 'icon-check');
-			}
-
-			confirmation.innerHTML = message;
-			addClass(confirmation, 'is-shown');
+var tabClick = function(e){
+	var old = document.querySelector('.nav-item.current');
+	var t = e.target;
+	if(!hasClass(t, 'faq-nav')) {
+		while(!hasClass(t.parentElement, 'faq-nav')) {
+			t = t.parentElement;
 		}
-		apiRequest(value, type, cb);
+		var num = parseInt(t.getAttribute('num'));
+		
+		pageGroup.style.left = '-'+num+'00%';
+		underline.style.left = num*tabWidth+'px';
+
+
+		removeClass(old, 'current');
+		addClass(t, 'current');
 	}
 	return false;
 }
-
-function apiRequest(value, type, callback){
-	var request = new XMLHttpRequest();
-	request.open("POST", "https://api.hacktj.org/interest/"+type);
-	request.onreadystatechange = function() {
-    if (this.readyState == 4) {
-    	return callback(this.status!==200, this.responseText);
-    }
-	}
-	var data = {};
-	data[type] = value;
-	request.setRequestHeader("Content-Type", "application/json");
-	request.send(JSON.stringify(data));
-}
-
-button.addEventListener('click', submit);
+faqNav.addEventListener("click", tabClick);
