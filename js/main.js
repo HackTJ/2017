8efa5d964d99c9ed16581a19e07dba21ca73d564
Peter Andringa <peter@andrin.ga>
2015-12-16 19:23:05 -0500

    add notification widget to homepage

--- js/main.js
+++ js/main.js
@@ -50,35 +50,6 @@ function getChildWithClass(el, className) {
 
 var isMobile = (window.innerWidth < 640);
 
-// FAQ Section
-// var faqContainer = document.querySelector('.faq-container');
-// var pageGroup = document.querySelector('ul.page-group');
-// var faqNav = document.querySelector('.faq-nav')
-// var pages = Array.prototype.slice.call(document.querySelectorAll('.faq-container .page'));
-// var tabs = document.querySelectorAll('.faq-nav .nav-item');
-// var underline = document.querySelector('.underline');
-// var tabWidth = tabs.item(0).clientWidth
-// underline.style.width = tabWidth+"px"
-
-// var tabClick = function(e){
-// 	var old = document.querySelector('.nav-item.current');
-// 	var t = e.target;
-// 	if(!hasClass(t, 'faq-nav')) {
-// 		while(!hasClass(t.parentElement, 'faq-nav')) {
-// 			t = t.parentElement;
-// 		}
-// 		var num = parseInt(t.getAttribute('num'));
-		
-// 		pageGroup.style.left = '-'+num+'00%';
-// 		underline.style.left = num*tabWidth+'px';
-
-
-// 		removeClass(old, 'current');
-// 		addClass(t, 'current');
-// 	}
-// 	return false;
-// }
-// faqNav.addEventListener("click", tabClick);
 var openQuestion = function(group, question, answer, tween){
   return function(e){
     var isOpen = toggleClass(group, 'is-open');
@@ -177,3 +148,103 @@ if(!isMobile){
           scrollController.destroy();
         });
 }
+
+// Temporary registration form code
+function buildForm(id){
+  console.log('#'+id+' input.contact')
+  var input = document.querySelector(id+' input.contact');
+  var button = document.querySelector(id+' button.submit');
+  var icon = document.querySelector(id+' button.submit .icon');
+  var confirmation = document.querySelector(id+' .confirmation-text');
+
+  // Regular Expressions
+  var phoneNumber = /^\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
+  var emailAddress = /^[^@\s]*@[a-z\-._]{1,}\.[a-z]{2,}$/i;
+
+  // Event Listeners
+  var validate = function(e){
+    if ( hasClass(button, 'is-disabled') && input.value ) {
+      removeClass(button, 'is-disabled');
+    } else if( !hasClass(button, 'is-disabled') && !input.value && !hasClass(button, 'is-done') ) {
+      addClass(button, 'is-disabled');
+    }
+
+    if ( hasClass(button, 'is-done') ) {
+      removeClass(button, 'is-done');
+      removeClass(icon, 'icon-check');
+    }
+    if ( hasClass(confirmation, 'is-shown') ) {
+      removeClass(confirmation, 'is-shown');
+    }
+
+    // Do nothing if the user presses 
+    if(event.keyCode === 13){
+      submit({});
+    }
+  }
+
+  input.addEventListener('keyup', validate);
+  input.addEventListener('focus', validate);
+  input.addEventListener('blur', validate);
+
+  var submit = function(e){
+    console.log('submit');
+    addClass(button, 'is-done');
+
+    var type = "invalid";
+
+    var value = input.value.trim();
+
+
+    if( value.match(phoneNumber) ) {
+      type = "phone"
+    }else if( value.match(emailAddress) ) {
+      type = "email";
+    }
+
+    if(type !== "email" && type !== "phone"){
+      confirmation.innerHTML = "Uh oh - that doesn't look like a phone number or email address.";
+      addClass(confirmation, 'is-shown');
+
+      addClass(icon, 'icon-close-circle');
+    } else {
+      addClass(icon, 'icon-config');
+      addClass(icon, 'icon-spin');
+      
+      // Universal Callback
+      var cb = function(err, message) {
+        removeClass(icon, 'icon-spin');
+        removeClass(icon, 'icon-config');
+        
+        if(err) {
+          addClass(icon, 'icon-close-circle');
+        } else {
+          addClass(icon, 'icon-check');
+        }
+
+        confirmation.innerHTML = message;
+        addClass(confirmation, 'is-shown');
+      }
+      apiRequest(value, type, cb);
+    }
+    return false;
+  }
+
+  function apiRequest(value, type, callback){
+    var request = new XMLHttpRequest();
+    request.open("POST", "https://api.hacktj.org/interest/"+type);
+    request.onreadystatechange = function() {
+      if (this.readyState == 4) {
+        return callback(this.status!==200, this.responseText);
+      }
+    }
+    var data = {};
+    data[type] = value;
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(JSON.stringify(data));
+  }
+
+  button.addEventListener('click', submit);
+}
+buildForm('#form1');
+buildForm('#form2');
